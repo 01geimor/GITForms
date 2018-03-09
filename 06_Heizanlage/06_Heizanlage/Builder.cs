0c638a5633f6c3bb1ac9aef7847b110970c380b3
01GeiMor_SPC <01geimor@rgtfo-me.it>
2018-03-09 12:30:34 +0100

    Label generator erstellt

--- 06_Heizanlage/06_Heizanlage/Builder.cs
+++ 06_Heizanlage/06_Heizanlage/Builder.cs
@@ -1,11 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace _06_Heizanlage
 {
     class Builder
     {
+        public Builder(int verschiebung)
+        {
+            this.verschiebung = verschiebung;
+        }
+        #region Labels
+        private GroupBox groupBox1;
+        private ProgressBar progressBar1;
+        private TrackBar trackBar1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        #endregion
+        private int verschiebung;
+
+        private void Generator(int i)
+        {
+            groupBox1 = new GroupBox();
+            trackBar1 = new TrackBar();
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(progressBar1);
+            groupBox1.Controls.Add(trackBar1);
+
+            groupBox1.Location = new Point(20,20);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(385, 139);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+
+            trackBar1.Location = new Point(121, 88);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(165, 45);
+            trackBar1.TabIndex = 0;
+
+            progressBar1.Location = new Point(121, 52);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(165, 23);
+            progressBar1.TabIndex = 1;
+
+            label1.Location = new Point(80, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 13);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+
+            label2.Location = new Point(292, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 13);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+
+            label3.Location = new Point(187, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 13);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+        }
+
+        #region Generator
+
+        public GroupBox Box()
+        {
+            GroupBox gb = new GroupBox();
+
+
+            return gb;
+        }
+
+        public Label[] Text(ref GroupBox gb ,int i)
+        {
+            Label[] l = new Label[3];
+
+            l[0] = new Label();
+            l[0].Location = new Point(80, 50+i*verschiebung);
+            l[0].Name = "lbl_min"+i;
+            l[0].Size = new Size(35, 13);
+            l[0].TabIndex = 2;
+            l[0].Text = "Min";
+
+            l[1] = new Label();
+            l[1].Location = new Point(292, 50+i*verschiebung);
+            l[1].Name = "lbl_max"+i;
+            l[1].Size = new Size(35, 13);
+            l[1].TabIndex = 3;
+            l[1].Text = "Max";
+
+            l[2] = new Label();
+            l[2].Location = new Point(187, 35+i*verschiebung);
+            l[2].Name = "lbl_Temp"+i;
+            l[2].Size = new Size(35, 13);
+            l[2].TabIndex = 4;
+            l[2].Text = "Temp";
+
+            gb.Controls.Add(l[0]);
+            gb.Controls.Add(l[1]);
+            gb.Controls.Add(l[2]);
+
+            return l;
+        }
+
+        #endregion
     }
 }
--- 06_Heizanlage/06_Heizanlage/House.cs
+++ 06_Heizanlage/06_Heizanlage/House.cs
@@ -18,6 +18,8 @@ namespace _06_Heizanlage
 
         public House(int h)
         {
+            Builder b = new Builder(100);
+            Label[] l;
 
             InitializeComponent();
         }
@@ -41,70 +43,8 @@ namespace _06_Heizanlage
             return a;
         }
 
-        private void Generator()
-        {
-            groupBox1 = new System.Windows.Forms.GroupBox();
-            trackBar1 = new System.Windows.Forms.TrackBar();
-            progressBar1 = new System.Windows.Forms.ProgressBar();
-            label1 = new System.Windows.Forms.Label();
-            label2 = new System.Windows.Forms.Label();
-            label3 = new System.Windows.Forms.Label();
-            groupBox1.SuspendLayout();
-            SuspendLayout();
+        
 
-            groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(progressBar1);
-            groupBox1.Controls.Add(trackBar1);
-            groupBox1.Location = new System.Drawing.Point(13, 13);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new System.Drawing.Size(385, 139);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
-            
-            trackBar1.Location = new System.Drawing.Point(121, 88);
-            trackBar1.Name = "trackBar1";
-            trackBar1.Size = new System.Drawing.Size(165, 45);
-            trackBar1.TabIndex = 0;
-            
-            
-            progressBar1.Location = new System.Drawing.Point(121, 52);
-            progressBar1.Name = "progressBar1";
-            progressBar1.Size = new System.Drawing.Size(165, 23);
-            progressBar1.TabIndex = 1;
-            
-            
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(80, 52);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(35, 13);
-            label1.TabIndex = 2;
-            label1.Text = "label1";
-            
-            
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(292, 52);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(35, 13);
-            label2.TabIndex = 3;
-            label2.Text = "label2";
-            
-            
-            label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(187, 36);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(35, 13);
-            label3.TabIndex = 4;
-            label3.Text = "label3";
-        }
-
-        private GroupBox groupBox1;
-        private ProgressBar progressBar1;
-        private TrackBar trackBar1;
-        private Label label3;
-        private Label label2;
-        private Label label1;
+        
     }
 }
